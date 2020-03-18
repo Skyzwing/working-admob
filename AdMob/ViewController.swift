a45e731f1f8c2cde23bcc69575647eec17f8042e
Surachet Y <aottapkub@hotmail.com>
2020-03-18 21:43:15 +0700

    Initial Commit

---
+++ AdMob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdMob
+//
+//  Created by Surachat Yaitammasan on 18/3/2563 BE.
+//  Copyright © 2563 Surachat Yaitammasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
